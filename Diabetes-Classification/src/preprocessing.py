75217b4f3155fdeef40d8bca8ecd010885bac964
João Pedro Rodrigues <162360449+Jottynha@users.noreply.github.com>
2026-03-24 16:32:04 -0300

    Update preprocessing.py

--- Diabetes-Classification/src/preprocessing.py
+++ Diabetes-Classification/src/preprocessing.py
@@ -1,17 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import OneHotEncoder
 def load_and_prepare_data(path, n_samples):
-    """
-    Carrega o dataset, aplica amostragem estratificada e divide em treino/teste.
-    """
-
-    # Carregar dataset
-    df = pd.read_csv(path)
-
+    df = pd.read_csv(path) 
     print(f"Dataset original: {len(df)} linhas")
-
-    # Amostragem estratificada
+    # amostragem estratificada
     if n_samples < len(df):
         df, _ = train_test_split(
             df,
@@ -19,31 +12,30 @@ def load_and_prepare_data(path, n_samples):
             stratify=df["diagnosed_diabetes"],
             random_state=42
         )
-
     df = df.reset_index(drop=True)
-
     print(f"Dataset reduzido: {len(df)} linhas")
-
-    # Separar features e alvo
+    # separar atributos e alvo
     X = df.drop(columns=[
         "diagnosed_diabetes",
         "diabetes_stage",
         "diabetes_risk_score"
     ]).values
-
     y = df["diagnosed_diabetes"].values
-
-    # Split treino/teste
+    # split treino/teste
     X_train, X_test, y_train, y_test = train_test_split(
         X, y,
         test_size=0.2,
         stratify=y,
         random_state=42
     )
+    # codificação de atributos categóricos
+    encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
+    X_train = encoder.fit_transform(X_train)
+    X_test = encoder.transform(X_test)    
 
-    # Verificação (opcional)
     print("\nDistribuição das classes:")
     print("Treino:", pd.Series(y_train).value_counts(normalize=True))
     print("Teste :", pd.Series(y_test).value_counts(normalize=True))
-
     return X_train, X_test, y_train, y_test
+
+load_and_prepare_data("Diabetes-Classification/data/diabetes_dataset.csv", n_samples=1000)
